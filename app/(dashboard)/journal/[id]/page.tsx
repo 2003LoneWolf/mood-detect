7d95231487d24b0f1e71086497bfefce1123bec8
Amar Murmu <iron57441@gmail.com>
2023-09-30 17:56:09 +0530

    feat: Editor, Autosave
    Added Editor component and autosave feature into textarea.

--- app/(dashboard)/journal/[id]/page.tsx
+++ app/(dashboard)/journal/[id]/page.tsx
@@ -1,8 +1,27 @@
+import Editor from '@/components/Editor'
+import { getUserByClerkID } from '@/utils/auth'
+import { prisma } from '@/utils/db'
 import React from 'react'
 
-const EntryPage = ({params}) => {
+const getEntry = async (id) => {
+  const user = await getUserByClerkID()
+  const entry = await prisma.journalEntry.findUnique({
+    where:{
+      userId_id:{
+        id,
+        userId:user.id,
+      }
+    },
+  })
+  return entry
+}
+
+const EntryPage = async ({params}) => {
+  const entry = await getEntry(params.id)
   return (
-    <div>{params.id}</div>
+    <div className='w-full h-full'>
+      <Editor entry={entry} />
+    </div>
   )
 }
 
---
+++ app/api/journal/[id]/route.ts
@@ -0,0 +1,19 @@
+import { getUserByClerkID } from "@/utils/auth"
+import { prisma } from "@/utils/db"
+
+export const PATCH =async (request,{params}) => {
+    const {content} = await request.json()
+    const user = await getUserByClerkID()
+    const updatedEntry = await prisma.journalEntry.update({
+        where:{
+            userId_id:{
+                userId:user.id,
+                id: params.id,
+            },
+        },
+        data:{
+            content,
+        },
+    })
+    return  NextResponse.json({data: updatedEntry})
+}
